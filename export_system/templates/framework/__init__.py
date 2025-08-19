27144ad2bbadc3025d639f83b80b3c0637774c7b
Andre Sant'Anna <andre@sant-anna.com>
2025-08-18 21:16:49 -0700

    missing imports

--- export_system/templates/framework/__init__.py
+++ export_system/templates/framework/__init__.py
@@ -5,6 +5,7 @@ from .graph_runner import GraphRunner
 from .exceptions import CauseExitException
 from .checkpoint import CheckpointManager, validate_checkpoint_config
 from .globals import Global
+from .multi_waiter import MultiWaiter
 from .dnne_exceptions import (
     DNNEError,
     NodeError, NodeConfigurationError, NodeConnectionError, NodeExecutionError,
@@ -22,6 +23,7 @@ __all__ = [
     'CheckpointManager',
     'validate_checkpoint_config',
     'Global',
+    'MultiWaiter',
     # DNNE Exception hierarchy
     'DNNEError',
     'NodeError', 'NodeConfigurationError', 'NodeConnectionError', 'NodeExecutionError',
